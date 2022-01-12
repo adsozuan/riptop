2369587b33d901e0aef6eeff8813212f754bce15
Adnan SOZUAN <adnan.sozuan@gmail.com>
2022-01-12 16:05:31 +0100

    Fix table render was producing exception.

--- src/ui/main_ui.cpp
+++ src/ui/main_ui.cpp
@@ -35,10 +35,11 @@ void RenderMainUi()
         system_times.UpdateCpuUsage();
 
         return vbox({
-            usage_gauge("CPU", static_cast<float>(system_times.cpu_usage())),
-            usage_gauge("MEM", static_cast<float>(mem_info.used_memory_percentage())),
-            usage_gauge("PGE", static_cast<float>(mem_info.used_page_memory_percentage())),
-        });
+                   usage_gauge("CPU", static_cast<float>(system_times.cpu_usage())),
+                   usage_gauge("MEM", static_cast<float>(mem_info.used_memory_percentage())),
+                   usage_gauge("PGE", static_cast<float>(mem_info.used_page_memory_percentage())),
+               }) |
+               notflex;
     };
 
     auto system_info_area = [&](const MemoryUsageInfo& mem_info) {
@@ -55,48 +56,37 @@ void RenderMainUi()
         MemoryUsageInfo mem_info;
 
         auto content = hbox(
-            {
-                usage_gauges_area(mem_info), 
-                vbox({separator(), separator(), separator()}), 
-                system_info_area(mem_info)
-            });
+            {usage_gauges_area(mem_info), vbox({separator(), separator(), separator()}), system_info_area(mem_info)});
         return content;
     });
 
-    auto process_table = Table({
-        {"ID", "USER", "PRI", "CPU%", "MEM", "THREAD", "DISK", "TIME", "PROCESS"},
-        {"25460", "ad", "4", "03.1%", "120.3 MB", "29", "0.0 MB/s", "00:01:30:39", "firefox.exe"},
-        {"25348", "ad", "8", "13.1%", "320.4 MB", "19", "0.0 MB/s", "00:01:40:11", "chrome.exe"},
-        {"25148", "ad", "4", "01.1%", "20.3 MB", "9", "0.0 MB/s", "00:01:45:11", "jezibella.exe"},
-        {"25111", "adnotnumber", "4", "11.1%", "20.3 MB", "4", "11.0 MB/s", "00:01:58:57",
-         "wonderful_long_application.exe"},
+    auto process_table_renderer = Renderer([&] {
+        auto process_table = Table({
+            {"ID", "USER", "PRI", "CPU%", "MEM", "THREAD", "DISK", "TIME", "PROCESS"},
+            {"25460", "ad", "4", "03.1%", "120.3 MB", "29", "0.0 MB/s", "00:01:30:39", "firefox.exe"},
+            {"25348", "ad", "8", "13.1%", "320.4 MB", "19", "0.0 MB/s", "00:01:40:11", "chrome.exe"},
+            {"25148", "ad", "4", "01.1%", "20.3 MB", "9", "0.0 MB/s", "00:01:45:11", "jezibella.exe"},
+            {"25111", "adnotnumber", "4", "11.1%", "20.3 MB", "4", "11.0 MB/s", "00:01:58:57",
+             "wonderful_long_application.exe"},
+        });
+
+        process_table.SelectColumns(0, 7).DecorateCells(align_right);
+        process_table.SelectCell(0, 0).DecorateCells(bgcolor(Color::CyanLight));
+        process_table.SelectRow(0).SeparatorVertical(EMPTY);
+        process_table.SelectRow(0).Decorate(bgcolor(Color::Green));
+        process_table.SelectRow(0).Decorate(color(Color::Black));
+
+        auto content = process_table.SelectRows(1, -1);
+        content.DecorateCellsAlternateRow(color(Color::Cyan), 2, 0);
+        content.DecorateCellsAlternateRow(color(Color::White), 2, 1);
+        return vbox({process_table.Render() | vscroll_indicator | frame});
     });
-
-    process_table.SelectColumns(0, 7).DecorateCells(align_right);
-    process_table.SelectCell(0, 0).DecorateCells(bgcolor(Color::CyanLight));
-    process_table.SelectRow(0).SeparatorVertical(EMPTY);
-    process_table.SelectRow(0).Decorate(bgcolor(Color::Green));
-    process_table.SelectRow(0).Decorate(color(Color::Black));
-
-    auto content = process_table.SelectRows(1, -1);
-    content.DecorateCellsAlternateRow(color(Color::Cyan), 2, 0);
-    content.DecorateCellsAlternateRow(color(Color::White), 2, 1);
-
-    auto process_table_renderer = Renderer([&] { return vbox({center(process_table.Render())}); });
-    // auto document = //
-    //    vbox({title(), separator(), global_usage(), separator(), vbox({center(process_table.Render()),
-    //    separator()})});
-
-    // Limit the size of the document to 80 char.
-    // document = document | size(WIDTH, LESS_THAN, 80);
 
     auto screen = ScreenInteractive::FitComponent();
 
     auto renderer = Renderer([&] {
-        return vbox({
-                   title->Render(), separator(), global_usage->Render(), separator(),
-                    //process_table_renderer->Render()
-               }) |
+        return vbox({title->Render(), separator(), global_usage->Render(), separator(),
+                     process_table_renderer->Render()}) |
                size(WIDTH, GREATER_THAN, 80) | border;
     });
 
