c7a5a5bc52fe57f35fcd7c6c24d0b30d0dd1ebaf
Adnan SOZUAN <adnan.sozuan@gmail.com>
2022-02-02 14:09:27 +0100

    Remove obsolete code.

--- src/services/acquisition_thread.cpp
+++ src/services/acquisition_thread.cpp
@@ -12,29 +12,10 @@
 using namespace riptop;
 using namespace ftxui;
 
-void riptop::AcquisitionThread::operator()(Sender<std::vector<ProcessInfo>> process_sender, Sender<SystemInfoDynamicData> system_info_sender, SystemDataService * system_data_service, Ui * main_ui)
+void riptop::AcquisitionThread::operator()(Sender<std::vector<ProcessInfo>> process_sender,
+                                           Sender<SystemInfoDynamicData>    system_info_sender,
+                                           SystemDataService* system_data_service, Ui* main_ui)
 {
-    while (true)
-    {
-        using namespace std::chrono_literals;
-        std::vector<ProcessInfo> processes;
-
-        SystemInfoDynamicData system_info_data;
-        system_data_service->Acquire(processes, system_info_data);
-
-        system_info_sender->Send(system_info_data);
-        process_sender->Send(processes);
-
-        std::this_thread::sleep_for(500ms);
-        main_ui->PostEvent();
-    }
-
-}
-void ProduceSystemData(Sender<std::vector<ProcessInfo>> process_sender,
-                       Sender<SystemInfoDynamicData> system_info_sender, SystemDataService* system_data_service,
-                       Ui* main_ui)
-{
-
     while (true)
     {
         using namespace std::chrono_literals;
